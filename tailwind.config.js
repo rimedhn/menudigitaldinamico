d097e9d119669cb584b94813939c73f1c0ac58aa
RM POS <indime.hn@gmail.com>
2025-09-15 01:23:19 -0600

    Add Tailwind CSS configuration with custom theme

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./productos.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        hutRed: "#C1272D",
+        hutGreen: "#7DBE31",
+        hutGray: "#E5E7EB",
+      },
+      fontFamily: {
+        hut: ['"Roboto"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
